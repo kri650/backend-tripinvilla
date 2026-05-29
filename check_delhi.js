1632d73bcb620617d7480d85a448cb36bb5eed61
Kritika Thakur <kritikathakur32671@gmail.com>
2026-05-29 16:32:53 +0530

    chore: remove ad-hoc check and migrate scripts

--- check_delhi.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import LocationMaster from './models/LocationMaster.js';
-import dotenv from 'dotenv';
-dotenv.config();
-
-await mongoose.connect(process.env.MONGO_URI);
-const locs = await LocationMaster.find({ locationName: /delhi/i });
-console.log(JSON.stringify(locs, null, 2));
-process.exit(0);
