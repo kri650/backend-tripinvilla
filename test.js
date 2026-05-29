886758f0a20f10d18bff5a21dabfde4a535c474f
Kritika Thakur <kritikathakur32671@gmail.com>
2026-05-29 16:25:08 +0530

    chore: remove ad-hoc test scripts

--- test.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-import PropertyRequest from './models/PropertyRequest.js';
-
-mongoose.connect('mongodb+srv://kritikathakur24_db_user:kritika123@kritika.jmr1rsb.mongodb.net/ngskillforge').then(async () => {
-  const reqs = await PropertyRequest.find({ room_type: /semi/i });
-  console.log(JSON.stringify(reqs, null, 2));
-  process.exit();
-});
