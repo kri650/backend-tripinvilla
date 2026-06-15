fb11d2839d27e3263a44872caa69679cf4ab7052
Kritika Thakur <kritikathakur32671@gmail.com>
2026-06-15 18:17:53 +0530

    done

--- utils/masterSync.js
+++ utils/masterSync.js
@@ -8,6 +8,7 @@ import RoomTypeMaster from '../models/RoomTypeMaster.js';
 import ExperienceMaster from '../models/ExperienceMaster.js';
 import AmenitiesMaster from '../models/AmenitiesMaster.js';
 import Property from '../models/Property.js';
+import PropertyMaster from '../models/PropertyMaster.js';
 
 // Normalize to Title Case: "villa" -> "Villa", "HOMESTAY" -> "Homestay"
 const toTitleCase = (str) => {
@@ -36,61 +37,213 @@ export const syncPropertyMasters = async (propertyData) => {
       }
     }
 
-    // 2. City
-    if (propertyData.city && typeof propertyData.city === 'string') {
-      const normalizedCity = toTitleCaseWords(propertyData.city);
-      const city = await CityMaster.findOne({ cityName: new RegExp(`^${normalizedCity}$`, 'i') });
-      if (!city) await CityMaster.create({ cityName: normalizedCity });
+    // A. Country First
+    let countryObj = null;
+    let countryIdVal = propertyData.countryId;
+    if (propertyData.country && typeof propertyData.country === 'string') {
+      const normalizedCountry = toTitleCaseWords(propertyData.country);
+      countryObj = await CountryMaster.findOne({ countryName: new RegExp(`^${normalizedCountry}$`, 'i') });
+      if (!countryObj) {
+        countryObj = await CountryMaster.create({ 
+          countryName: normalizedCountry, 
+          dialCode: '-', 
+          currencyCode: '-', 
+          currencySymbol: '-' 
+        });
+      }
+      countryIdVal = countryObj._id;
+    } else if (countryIdVal) {
+      countryObj = await CountryMaster.findById(countryIdVal);
     }
 
-    // 3. Location
-    if (propertyData.location && typeof propertyData.location === 'string') {
-      const loc = await LocationMaster.findOne({ locationName: new RegExp(`^${propertyData.location}$`, 'i') });
-      const mappedLandmarks = Array.isArray(propertyData.landmarks) 
-        ? propertyData.landmarks.map(lm => ({
-            name: lm.landmark_name || lm.name || 'Unknown Landmark',
-            popularity: lm.landmark_type || lm.type || 'Tourist Popular',
-            images: lm.landmark_image_url || lm.image || lm.img ? [lm.landmark_image_url || lm.image || lm.img] : []
-          }))
-        : [];
-
-      if (!loc) {
-        await LocationMaster.create({ 
-          locationName: propertyData.location, 
-          locationType: 'Area',
-          landmarks: mappedLandmarks
+    // B. State Second
+    let stateObj = null;
+    let stateIdVal = propertyData.stateId;
+    if (propertyData.state && typeof propertyData.state === 'string') {
+      const normalizedState = toTitleCaseWords(propertyData.state);
+      stateObj = await StateMaster.findOne({ stateName: new RegExp(`^${normalizedState}$`, 'i') });
+      if (!stateObj) {
+        stateObj = await StateMaster.create({ 
+          stateName: normalizedState,
+          countryId: countryIdVal || undefined
         });
-      } else if (mappedLandmarks.length > 0) {
-        // Append new landmarks that don't already exist in the location by name
-        let modified = false;
-        const existingNames = loc.landmarks.map(l => (l.name || '').toLowerCase());
-        
-        for (const newLm of mappedLandmarks) {
-          if (!existingNames.includes(newLm.name.toLowerCase())) {
-            loc.landmarks.push(newLm);
-            existingNames.push(newLm.name.toLowerCase());
-            modified = true;
-          }
+      } else {
+        if (countryIdVal && String(stateObj.countryId) !== String(countryIdVal)) {
+          stateObj.countryId = countryIdVal;
+          await stateObj.save();
         }
-        
-        if (modified) {
-          await loc.save();
-        }
+      }
+      stateIdVal = stateObj._id;
+    } else if (stateIdVal) {
+      stateObj = await StateMaster.findById(stateIdVal);
+      if (stateObj && countryIdVal && String(stateObj.countryId) !== String(countryIdVal)) {
+        stateObj.countryId = countryIdVal;
+        await stateObj.save();
       }
     }
 
-    // 4. State
-    if (propertyData.state && typeof propertyData.state === 'string') {
-      const normalizedState = toTitleCaseWords(propertyData.state);
-      const state = await StateMaster.findOne({ stateName: new RegExp(`^${normalizedState}$`, 'i') });
-      if (!state) await StateMaster.create({ stateName: normalizedState });
+    // C. City Third
+    let cityObj = null;
+    let cityIdVal = propertyData.cityId;
+    if (propertyData.city && typeof propertyData.city === 'string') {
+      const normalizedCity = toTitleCaseWords(propertyData.city);
+      const query = { cityName: new RegExp(`^${normalizedCity}$`, 'i') };
+      if (stateIdVal) {
+        query.stateId = stateIdVal;
+      }
+      cityObj = await CityMaster.findOne(query);
+      if (!cityObj) {
+        cityObj = await CityMaster.create({ 
+          cityName: normalizedCity,
+          stateId: stateIdVal || undefined,
+          countryId: countryIdVal || undefined
+        });
+      } else {
+        let updated = false;
+        if (stateIdVal && String(cityObj.stateId) !== String(stateIdVal)) {
+          cityObj.stateId = stateIdVal;
+          updated = true;
+        }
+        if (countryIdVal && String(cityObj.countryId) !== String(countryIdVal)) {
+          cityObj.countryId = countryIdVal;
+          updated = true;
+        }
+        if (updated) await cityObj.save();
+      }
+      cityIdVal = cityObj._id;
+    } else if (cityIdVal) {
+      cityObj = await CityMaster.findById(cityIdVal);
+      if (cityObj) {
+        let updated = false;
+        if (stateIdVal && String(cityObj.stateId) !== String(stateIdVal)) {
+          cityObj.stateId = stateIdVal;
+          updated = true;
+        }
+        if (countryIdVal && String(cityObj.countryId) !== String(countryIdVal)) {
+          cityObj.countryId = countryIdVal;
+          updated = true;
+        }
+        if (updated) await cityObj.save();
+      }
     }
 
-    // 5. Country
-    if (propertyData.country && typeof propertyData.country === 'string') {
-      const normalizedCountry = toTitleCaseWords(propertyData.country);
-      const country = await CountryMaster.findOne({ countryName: new RegExp(`^${normalizedCountry}$`, 'i') });
-      if (!country) await CountryMaster.create({ countryName: normalizedCountry, dialCode: '-', currencyCode: '-', currencySymbol: '-' });
+    // D. Location (Area) Fourth
+    let locObj = null;
+    let locationIdVal = propertyData.locationId;
+
+    const hierarchyParts = [];
+    if (cityObj) hierarchyParts.push(cityObj.cityName);
+    if (stateObj) hierarchyParts.push(stateObj.stateName);
+    if (countryObj) hierarchyParts.push(countryObj.countryName);
+    const parentLocationStr = hierarchyParts.join(' → ');
+
+    const mappedLandmarks = Array.isArray(propertyData.landmarks) 
+      ? propertyData.landmarks.map(lm => ({
+          name: lm.landmark_name || lm.name || 'Unknown Landmark',
+          popularity: lm.landmark_type || lm.type || 'Tourist Popular',
+          images: lm.landmark_image_url || lm.image || lm.img ? [lm.landmark_image_url || lm.image || lm.img] : []
+        }))
+      : [];
+
+    if (propertyData.location && typeof propertyData.location === 'string') {
+      const locName = propertyData.location;
+      const query = { locationName: new RegExp(`^${locName}$`, 'i') };
+      if (parentLocationStr) {
+        query.parentLocation = new RegExp(`^${parentLocationStr}$`, 'i');
+      }
+      locObj = await LocationMaster.findOne(query);
+      
+      if (!locObj) {
+        locObj = await LocationMaster.create({ 
+          locationName: locName, 
+          locationType: 'Area',
+          parentLocation: parentLocationStr || undefined,
+          landmarks: mappedLandmarks
+        });
+      } else {
+        let updated = false;
+        if (parentLocationStr && locObj.parentLocation !== parentLocationStr) {
+          locObj.parentLocation = parentLocationStr;
+          updated = true;
+        }
+        if (mappedLandmarks.length > 0) {
+          const existingNames = locObj.landmarks.map(l => (l.name || '').toLowerCase());
+          for (const newLm of mappedLandmarks) {
+            if (!existingNames.includes(newLm.name.toLowerCase())) {
+              locObj.landmarks.push(newLm);
+              existingNames.push(newLm.name.toLowerCase());
+              updated = true;
+            }
+          }
+        }
+        if (updated) await locObj.save();
+      }
+      locationIdVal = locObj._id;
+    } else if (locationIdVal) {
+      locObj = await LocationMaster.findById(locationIdVal);
+      if (locObj) {
+        let updated = false;
+        if (parentLocationStr && locObj.parentLocation !== parentLocationStr) {
+          locObj.parentLocation = parentLocationStr;
+          updated = true;
+        }
+        if (mappedLandmarks.length > 0) {
+          const existingNames = locObj.landmarks.map(l => (l.name || '').toLowerCase());
+          for (const newLm of mappedLandmarks) {
+            if (!existingNames.includes(newLm.name.toLowerCase())) {
+              locObj.landmarks.push(newLm);
+              existingNames.push(newLm.name.toLowerCase());
+              updated = true;
+            }
+          }
+        }
+        if (updated) await locObj.save();
+      }
+    }
+
+    // E. Save relation IDs back to propertyData object to support upstream route creations
+    if (countryIdVal) propertyData.countryId = countryIdVal;
+    if (stateIdVal) propertyData.stateId = stateIdVal;
+    if (cityIdVal) propertyData.cityId = cityIdVal;
+    if (locationIdVal) propertyData.locationId = locationIdVal;
+    if (countryObj) propertyData.countryName = countryObj.countryName;
+    if (stateObj) propertyData.stateName = stateObj.stateName;
+    if (cityObj) propertyData.cityName = cityObj.cityName;
+    if (locObj) propertyData.locationName = locObj.locationName;
+
+    // F. Direct DB Update for Property and PropertyMaster documents to ensure robustness
+    const updates = {};
+    if (countryIdVal) {
+      updates.countryId = countryIdVal;
+      if (countryObj) updates.countryName = countryObj.countryName;
+    }
+    if (stateIdVal) {
+      updates.stateId = stateIdVal;
+      if (stateObj) updates.stateName = stateObj.stateName;
+    }
+    if (cityIdVal) {
+      updates.cityId = cityIdVal;
+      if (cityObj) updates.cityName = cityObj.cityName;
+    }
+    if (locationIdVal) {
+      updates.locationId = locationIdVal;
+      if (locObj) updates.locationName = locObj.locationName;
+    }
+
+    if (Object.keys(updates).length > 0) {
+      if (propertyData._id && mongoose.isValidObjectId(propertyData._id)) {
+        await Property.findByIdAndUpdate(propertyData._id, updates);
+        await PropertyMaster.findByIdAndUpdate(propertyData._id, updates);
+      }
+      if (propertyData.propertyNo) {
+        await Property.findOneAndUpdate({ propertyNo: propertyData.propertyNo }, updates);
+        await PropertyMaster.findOneAndUpdate({ propertyNo: propertyData.propertyNo }, updates);
+      }
+      const propName = propertyData.name || propertyData.propertyName;
+      if (propName) {
+        await Property.findOneAndUpdate({ name: propName }, updates);
+        await PropertyMaster.findOneAndUpdate({ propertyName: propName }, updates);
+      }
     }
 
     // 6. Amenities
